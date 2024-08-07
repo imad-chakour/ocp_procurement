8d4a97ae347c16bf59df9c7791037a11a26275cc
yg-commits <chakourimad01@gmail.com>
2024-08-07 21:47:30 +0100

    Fix DATABASE_URL configuration

--- app/database.py
+++ app/database.py
@@ -1,13 +1,15 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgre2024@localhost/OCPproject"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL environment variable is not set.")
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db_conn():
